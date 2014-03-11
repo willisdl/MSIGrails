2d24688f2e1d4f935424ccaeb9e21af79ac8cf9a
David Willis <david.l.willis@me.com>
2014-03-11 14:32:57 -0400

    Updated datasource with new environment

--- MSIGrails/grails-app/conf/DataSource.groovy
+++ MSIGrails/grails-app/conf/DataSource.groovy
@@ -12,13 +12,20 @@ hibernate {
 }
 // environment specific settings
 environments {
-    development {
+    development_pc {
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
 			url = "jdbc:oracle:thin:@192.168.2.20:1521:orcl"
         }
     }
+	development_mac {
+		dataSource {
+			dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+			//url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			url = "jdbc:oracle:thin:@192.168.2.20:1521:orcl"
+		}
+	}
     test {
         dataSource {
             //dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
